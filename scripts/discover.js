ac552e0262e06c2c2d61aaf9cef8607545191a9e
serik_lav <serik_lav@mail.ru>
2020-07-23 23:11:51 +0300

    change all project

---
+++ scripts/discover.js
@@ -0,0 +1,15 @@
+$(() => {
+    $('.discover-people').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        centerPadding: 0,
+        arrows : false,
+    });
+
+    $('.discover-service').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerPadding: 0,
+        arrows : false,
+    });
+});
