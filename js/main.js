13b200912f44fd27d1a9a25f76e63301db6d1c7c
serik_lav <serik_lav@mail.ru>
2020-07-23 18:46:49 +0300

    fix resize

--- js/main.js
+++ js/main.js
@@ -16,3 +16,12 @@ $(document).ready(function(){
         }
     })
 });
+
+function getSize() {
+    return {
+        width: window.innerWidth || document.documentElement.clientWidth ||
+            document.body.clientWidth,
+        height: window.innerHeight || document.documentElement.clientHeight ||
+            document.body.clientHeight,
+    };
+}
